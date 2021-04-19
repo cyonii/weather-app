bcfe256d024261c74de16c76d643c9fe7d20b793
CY Kalu <cy23@tutanota.com>
2021-04-19 04:59:14 +0100

    Specify mode in `fetch` options

--- src/js/weather.js
+++ src/js/weather.js
@@ -2,5 +2,5 @@ export default async function getWeatherData(location, apiKey) {
   const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
   const fullURL = `${baseURL}?q=${location}&APPID=${apiKey}`;
 
-  return fetch(fullURL).then((response) => response.json());
+  return fetch(fullURL, { mode: 'cors' }).then((response) => response.json());
 }
