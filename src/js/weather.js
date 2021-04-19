7ce3b8f18ad552a270a8cd391a94e3ac829c6af5
CY Kalu <cy23@tutanota.com>
2021-04-19 13:14:28 +0100

    Use secure https protocol for API endpoint

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,5 +1,5 @@
 export default async function getWeatherData(location, unit, apiKey) {
-  const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+  const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
   const fullURL = `${baseURL}?q=${location}&APPID=${apiKey}&units=${unit}`;
 
   return fetch(fullURL, { mode: 'cors' }).then((response) => response.json());
